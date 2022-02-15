2087bb67a5f98ff7319d518d385dbe100c8ddefe
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2022-02-15 17:28:18 +0400

    Update n4.cpp

--- n4.cpp
+++ n4.cpp
@@ -31,7 +31,7 @@ int main(void)
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-	//freopen("26-4.txt", "r", stdin);
+	freopen("26-4.txt", "r", stdin);
 	
 	ll even = 0;
 	ll odd = 0;
@@ -84,6 +84,23 @@ int main(void)
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
